20266ff085d0e1658bdd1efccd17504fdb9f1faa
Twinkle Sharma <74674378+hacker795@users.noreply.github.com>
2025-12-12 12:40:07 -0500

    Update index.php

--- webroot/index.php
+++ webroot/index.php
@@ -1,6 +1,9 @@
-<?php echo "Welcome to FuzzCorp Web Portal"; ?>
 <?php
-if (isset($_GET['debug'])) {
-    echo "Debug mode enabled";
+
+echo "Welcome to FuzzCorp Web Portal";
+
+if (isset($_GET['debug']) && $_GET['debug'] == "1") {
+    echo "\nSECRET: Debug mode is ON!\nThis is sensitive data!\n";
 }
+
 ?>
